b3934ebf57219a711e4b23cdb8befeb0b7788608
John Bunting <john.lee.bunting@gmail.com>
2021-01-08 10:22:04 -0600

    Simple Work

--- src/core/encounter.py
+++ src/core/encounter.py
@@ -1,10 +1,43 @@
+"""
 
+    encounter.py
+
+    TODO
+    Build out base encounter tool
+
+
+"""
 
 class Encounter( object ):
-    """ An Encounter, involving PCs and NPCs
+    """ An Encounter, involving PCs and NPCs """
 
-    :param name: A string, the person's name.
-    :param age: An int, the person's age.
-    """
+    __slots__ = [ "__pcs", "__npcs" ]
+
+    
     def __init__( self ):
+        
+        self.__npcs = []
+        self.__pcs = []
+
+
+    def add_pc( self, name, initiative ):
+        """ An Encounter, involving PCs and NPCs
+
+        :param name: A string, the person's name.
+        :param age: An int, the person's age.
+        """
+
+        self.__pcs.append( 
+            [
+                name,
+                initiative
+            ]
+        )
+
+
+    def initialize( self ):
+        print( "hi" )
+
+    
+    def step( self ):
         print( "hi" )
--- src/main.py
+++ src/main.py
@@ -11,3 +11,6 @@ from core.encounter import Encounter
 dir_path = os.path.dirname( os.path.realpath( __file__ ) )
 
 e = Encounter()
+e.add_pc( "test", 1 )
+e.initialize()
+e.step()
