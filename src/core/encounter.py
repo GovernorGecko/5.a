8ccf14b076dc0029ba685df92d330ad5c9a9b82d
John Bunting <john.lee.bunting@gmail.com>
2021-01-07 23:17:44 -0600

    Added Encounter and Main files.

---
+++ src/core/encounter.py
@@ -0,0 +1,10 @@
+
+
+class Encounter( object ):
+    """ An Encounter, involving PCs and NPCs
+
+    :param name: A string, the person's name.
+    :param age: An int, the person's age.
+    """
+    def __init__( self ):
+        print( "hi" )
---
+++ src/main.py
@@ -0,0 +1,13 @@
+'''
+
+    Main
+
+'''
+
+import os
+
+from core.encounter import Encounter
+
+dir_path = os.path.dirname( os.path.realpath( __file__ ) )
+
+e = Encounter()
