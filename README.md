71eeb8969221ba2b821d02e60462de2bbcc99cc6
John Bunting <john.lee.bunting@gmail.com>
2021-01-09 11:11:30 -0600

    Working Encounter

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 5.a
 DnD 5th Edition Alternate
+
+
+Injury Dice?
+Roll Hit Dice to prevent?
+Slower regain from rest?
--- src/core/encounter.py
+++ src/core/encounter.py
@@ -27,32 +27,65 @@ class Encounter( object ):
     """
 
 
-    __slots__ = [ "__characters" ]
+    __slots__ = [ "__characters", "__turn" ]
 
     
     def __init__( self ):        
         self.__characters = []
+        self.__turn = -1
 
 
     def __str__( self ):
-        return str( self.__characters )
+        return "\n".join( map( str, self.__characters ) )
 
 
     def add( self, character ):
-        """ An Encounter, involving PCs and NPCs
+        """ Adds a Character
 
-        :param name: A string, the person's name.
-        :param age: An int, the person's age.
+        :param character: A Character class
         """
 
-        character.uuid = uuid.uuid1
+        # Create a UUID for this Character
+        character.uuid = uuid.uuid1()
 
+        # Add to our list
         self.__characters.append( character )
 
 
+    def get_current_turn( self ):
+        """ Returns the Character object for who's turn it currently is.
+
+        """
+
+        # Turn?
+        if self.__turn >= 0 and self.__turn < len( self.__characters ):
+            return self.__characters[ self.__turn ]
+        return None
+
+
     def initialize( self ):
-        print( "hi" )
+        """ Roll Initiative, store it
+
+        """
+
+        # We have enough characters?
+        if len( self.__characters ) == 0:
+            return False
+
+        # Reset local turn
+        self.__turn = -1
+
+        # Iterate our characters, rolling init
+        for character in self.__characters:
+            character.initiative = character._initiative.roll()
 
     
     def step( self ):
-        print( "hi" )
+        """ Step through the turns.
+
+        """
+        
+        # Next turn!
+        self.__turn = self.__turn + 1
+        if self.__turn >= len( self.__characters ):
+            self.__turn = 0
